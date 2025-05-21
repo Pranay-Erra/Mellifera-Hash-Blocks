cd82f5492ec551a3d22539c2f9b22ccb5a897f7e
Pranay-Erra <pranayerra2003@gmail.com>
2025-05-21 15:07:01 +0530

    ok

--- client/src/contactForm.js
+++ client/src/contactForm.js
@@ -24,7 +24,7 @@ const ContactForm = () => {
     e.preventDefault();
     if (validate()) {
       try {
-        const response = await fetch("http://localhost:5000/send-message", {
+        const response = await fetch("https://mellifera-hash-blocks.onrender.com/send-message", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(formData),
